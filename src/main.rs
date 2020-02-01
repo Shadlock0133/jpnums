fe1ecd22f66ca91fd1223087681e68b5105d9396
Aurora <tomek-kubel2@wp.pl>
2020-02-01 18:29:25 +0100

    modernize

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,55 @@
-extern crate rand;
-
 use rand::Rng;
-use std::collections::HashSet;
 
-const LIMIT: usize = 100;
+struct Reading {
+    digits: [&'static str; 10],
+    ten: &'static str,
+    hundred: &'static str,
+}
 
-// On readings from 0 to 10 and 100
-const ON: [&'static str; 12] =
-["れい", "いち", "に", "さん", "し", "ご", "ろく", "しち", "はち", "きゅう", "じゅう", "ひゃく"];
+// On readings from 0 to 9
+const ON: Reading = Reading {
+    digits: ["れい", "いち", "に", "さん", "し", "ご", "ろく", "しち", "はち", "きゅう"],
+    ten: "じゅう",
+    hundred: "ひゃく",
+};
 
-fn jap_numeral(number: u16) -> String {
+fn jap_numeral(number: u32) -> Result<String, ()> {
     let number = number as usize;
     let nums = ON;
     let mut string = String::new();
-    if number >= 1000 { return "Out of range".to_owned(); }
-    if number >=  200 { string.push_str(nums[number % 1000 / 100]); }
-    if number >=  100 { string.push_str(nums[11]); }
-    if number >=   20 { string.push_str(nums[number % 100 / 10]); }
-    if number >=   10 { string.push_str(nums[10]); }
-    if number !=    0 { string.push_str(nums[number % 10]); }
-    else              { string.push_str(nums[0]) };
-    string
+    if number >= 1000 { return Err(()); }
+    if number >=  200 { string.push_str(nums.digits[number % 1000 / 100]); }
+    if number >=  100 { string.push_str(nums.hundred); }
+    if number >=   20 { string.push_str(nums.digits[number % 100 / 10]); }
+    if number >=   10 { string.push_str(nums.ten); }
+    if number >=    1 { string.push_str(nums.digits[number % 10]); }
+    else              { string.push_str(nums.digits[0]) };
+    Ok(string)
 }
 
 fn main() {
-    let mut counter = HashSet::with_capacity(LIMIT);
     let mut rng = rand::thread_rng();
     println!("Kon'nichiwa!");
-    for _ in 0..LIMIT {
-        let mut number;
-        loop {
-            number = rng.gen_range::<u16>(0, 100);
-            if !counter.contains(&number) { break; }
-        }
-        println!("{}", jap_numeral(number));
-        // println!("Debug: {}; {:?}", number, counter);
-        let mut input = String::new();
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        let input = input.trim()
-            .parse::<u16>()
-            .expect("Failed to parse input");
-        if input == number {
-            counter.insert(number);
-            println!("Correct");
-        } else {
-            println!("Try again");
+    // TODO: choosable difficulty
+    loop {
+        let number = rng.gen_range(0, 100);
+        println!("{}", jap_numeral(number).unwrap());
+
+        'valid_anwser: loop {
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            let input: u32 = input.trim()
+                .parse()
+                .expect("Failed to parse input");
+
+            if input == number {
+                println!("Correct");
+                break 'valid_anwser;
+            } else {
+                println!("Try again");
+            }
         }
     }
-    println!("Congratulation");
 }
